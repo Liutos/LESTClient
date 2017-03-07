e0dbbd60e204aa70b96eb1193bfa002260a6d863
LiuTao <liutao@shefenqi.com>
2017-03-07 19:39:19 +0800

    实现点击按钮后向服务端接口发送输入框中内容的功能

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,19 @@
-(function () {
-  var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Welcome to LESTClient!'
-    },
-    methods: {
-      echoMessage: function () {
-        console.log('Content of message is ' + this.message);
-      }
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Welcome to LESTClient!'
+  },
+  methods: {
+    echoMessage: function () {
+      Vue.http.post('/api/request', {
+        url: this.message
+      }, {
+        'Content-Type': 'application/json'
+      }).then(response => {
+        console.log('SUCCEED');
+      }, response => {
+        console.log('FAIL');
+      });
     }
-  });
-})();
+  }
+});
