0c617e35e61be9d4b3453b337a5877e846bc1cc3
Liutos <mat.liutos@gmail.com>
2017-03-29 10:09:48 +0800

    修复bug
    应当使用vector类型做判定，而不是simple-array

--- http_client.lisp
+++ http_client.lisp
@@ -3,7 +3,7 @@
 (defun http-request (uri &rest args)
   (multiple-value-bind (body code headers)
       (apply #'drakma:http-request uri args)
-    (let ((body (if (typep body '(simple-array (unsigned-byte 8)))
+    (let ((body (if (typep body '(vector (unsigned-byte 8)))
                     (flexi-streams:octets-to-string
                      body
                      :external-format :utf-8)
